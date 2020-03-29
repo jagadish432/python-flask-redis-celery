d17e14013b4d3e88a30285a69b16c4d980347102
Jagadeesh Dachepalli <34298777+jagadish432@users.noreply.github.com>
2020-03-29 15:26:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ MAIL_SERVER = 'smtp.googlemail.com'
 MAIL_PORT = 587
 MAIL_USE_TLS = True
 MAIL_USERNAME = 'maheshbabu4329@gmail.com'
-MAIL_PASSWORD = '9030648036'
+MAIL_PASSWORD = '' #change when running in the local
 
 
 # broker details in the case of redis
